abc3ba1ead91272cea40ef46b928c0bd565d5dbf
abner-ma <969023674@qq.com>
2018-03-18 22:36:58 +0800

    add daemon.

--- ser/src/server.c
+++ ser/src/server.c
@@ -1,7 +1,38 @@
-#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/param.h>
+#include<sys/types.h>
+#include<sys/stat.h>
 
-int main()
+int init_daemon()
 {
-    printf("This is a server.\n");
+	int pid;
+	int i;
+	if(pid = fork())
+	{
+		exit(0);
+	}
+	else if(pid < 0)
+	{
+		exit(1);
+	}
+	setsid();
+	if(pid = fork())
+	{
+		exit(0);
+	}
+	else if(pid < 0)
+	{
+		exit(1);
+	}
+	for(i = 0;i < NOFILE;i++)
+		close(i);
+	chdir("/tmp");
+	umask(0);
+	return 0;
+}
+int main(int argc,char * argv)
+{
     return 0;
 }
